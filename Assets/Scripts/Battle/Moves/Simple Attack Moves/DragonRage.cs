227c3242f4917e92034a0451a74cf5e5169d7cb1
Joseph Collado <sourpina@gmail.com>
2023-05-19 16:51:57 -0400

    Removed execute override
    Removed the execute override which should have been removed earlier when fixed damage move execution was refactored.

--- Assets/Scripts/Battle/Moves/Simple Attack Moves/DragonRage.cs
+++ Assets/Scripts/Battle/Moves/Simple Attack Moves/DragonRage.cs
@@ -17,19 +17,4 @@ public class DragonRage : SimpleAttackMove
         dealsFixedDamage = true;
         fixedDamage = 40;
     }
-
-    /* This move deals 40 damage no matter what
-     * It does not take into account resistances and weaknesses */
-    public override IEnumerator Execute(BattlePokemon user, BattlePokemon opponent)
-    {
-        yield return Battle.StartCoroutine(OnUsed(user));
-
-        if(opponent.IsSemiInvulnerable || !AccuracyCheck(user, opponent))
-            yield return Battle.StartCoroutine(OnMissed(user));
-        else
-            yield return Battle.StartCoroutine(opponent.RecieveDamge(40, Type));
-
-        yield return new WaitForSeconds(60 / 60f);
-        EndMove(user, opponent);
-    }
 }
